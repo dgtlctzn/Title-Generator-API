f188d5c7714229d2299e0d6a7e14543386551fe5
Joseph Perry <josephperry720@gmail.com>
2020-12-23 10:49:37 -0500

    converted get route to accept query params

--- src/main/java/com/example/demo/api/SongTitleController.java
+++ src/main/java/com/example/demo/api/SongTitleController.java
@@ -2,10 +2,16 @@ package com.example.demo.api;
 
 import com.example.demo.model.SongTitle;
 import com.example.demo.service.SongTitleService;
+import com.sun.istack.internal.NotNull;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.lang.NonNull;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
+import javax.websocket.server.PathParam;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 @RequestMapping("/api/v1/songtitle")
@@ -20,19 +26,25 @@ public class SongTitleController {
     }
 
     @PostMapping
-    public void addSongTitle(@RequestBody SongTitle songTitle) {
+    public void addSongTitle(@Validated @NonNull @RequestBody SongTitle songTitle) {
         songTitleService.addSongTitle(songTitle);
     }
 
     @GetMapping
-    public List<SongTitle> getAllSongTitles() {
+    public List<SongTitle> getAllSongTitles(@RequestParam("id") Optional<UUID> id) {
+        if (id.isPresent()) {
+            List<SongTitle> foundList = new ArrayList<>();
+            SongTitle foundId = songTitleService.getSongTitleById(id.get()).orElse(null);
+            foundList.add(foundId);
+            return foundList;
+        }
         return songTitleService.getAllSongTitles();
     }
 
-    @GetMapping(path = "{id}")
-    public SongTitle getSongTitleById(@PathVariable("id") UUID id) {
-        return songTitleService.getSongTitleById(id).orElse(null);
-    }
+//    @GetMapping(path = "{id}")
+//    public SongTitle getSongTitleById(@PathVariable("id") UUID id) {
+//        return songTitleService.getSongTitleById(id).orElse(null);
+//    }
 
     @DeleteMapping(path = "{id}")
     public void deleteSongTitleById(@PathVariable("id") UUID id) {
@@ -40,7 +52,7 @@ public class SongTitleController {
     }
 
     @PutMapping(path = "{id}")
-    public void updateSongTitle(@PathVariable("id") UUID id, @RequestBody SongTitle songTitleToUpdate) {
+    public void updateSongTitle(@PathVariable("id") UUID id, @Validated @NonNull @RequestBody SongTitle songTitleToUpdate) {
         songTitleService.updateSongTitle(id, songTitleToUpdate);
     }
  }
--- src/main/java/com/example/demo/model/SongTitle.java
+++ src/main/java/com/example/demo/model/SongTitle.java
@@ -1,6 +1,8 @@
 package com.example.demo.model;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.sun.istack.internal.NotNull;
+import org.springframework.lang.NonNull;
 
 import java.util.UUID;
 
